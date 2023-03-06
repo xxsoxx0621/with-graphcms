8d8e163f01d05905daea234f7a5d7da537869a45
배소현 <sohyeon0621@theopusone.com>
2023-03-06 18:38:48 +0900

    feature : add CartContext

---
+++ src/lib/context/Cart/index.js
@@ -0,0 +1,9 @@
+import {createContext} from 'react'
+
+const CartContext = createContext({
+    items   : {},
+    setItems: () => {
+    },
+});
+
+export default CartContext;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,21 @@
 import {Box, ChakraProvider, Flex} from "@chakra-ui/react";
 import NavBar from '@/components/NavBar'
+import CartContext from '@/lib/context/Cart'
+import {useState} from 'react'
 
 function MyApp({Component, pageProps}) {
+    const [items, setItems] = useState([]);
+
     return (
         <ChakraProvider>
-            <Flex w="full" minH="100vh" bgColor="gray.100">
-                <NavBar/>
-                <Box maxW="70vw" m="auto">
-                    <Component {...pageProps}/>
-                </Box>
-            </Flex>
+            <CartContext.Provider value={{items, setItems}}>
+                <Flex w="full" minH="100vh" bgColor="gray.100">
+                    <NavBar/>
+                    <Box maxW="70vw" m="auto">
+                        <Component {...pageProps}/>
+                    </Box>
+                </Flex>
+            </CartContext.Provider>
         </ChakraProvider>
     )
 }
